1fd8820353b9fdd56a4bad0306c206a0f8b4af42
sriley <sarahriley.dev@gmail.com>
2020-01-31 13:40:03 -0500

    names complete

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BinarySearchTree
 
 start_time = time.time()
 
@@ -11,14 +12,24 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Previous solution: nested loops = O(n^2)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+binary_search = BinarySearchTree(names_1[0])
+
+# using a binary search tree, we only have to make one pass through both lists, so this is an O(n) solution
+for name in names_1[1:]:
+    binary_search.insert(name)
+for duplicate in names_2:
+    if binary_search.contains(duplicate):
+        duplicates.append(duplicate)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
